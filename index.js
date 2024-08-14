20e91b025f8083abc8f5e01c3d4781ff8e96ac59
Sanaghufran <sanaghufran2005@gmail.com>
2024-08-14 12:55:10 +0530

    Action names defined as constants

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import {createStore , applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+const inc = "increment";
+const dec = "decrement";
+const incByAmount = "incrementByAmount"
 const store = createStore(reducer,applyMiddleware(logger.default));
+
 function reducer(state = {amount:1}, action){//initial value of state is assigned as 1
-    if(action.type === 'increment'){
+    if(action.type === inc){
         //immutability
         //state.amount = state.amount+1
         return {amount:state.amount+1};// this will be commented in immutability
     }
-    if(action.type === 'decrement'){
+    if(action.type === dec){//manlo aap decrement ka r type krna bhul gae to ye chalega hi nhi iske liye action names ko constant banado
       
         return {amount:state.amount-1};// this will be commented in immutability
     }
-    if(action.type === 'incrementByAmount'){
+    if(action.type === incByAmount){
        
         return {amount:state.amount+action.payload};// this will be commented in immutability
     }
@@ -19,7 +23,7 @@ function reducer(state = {amount:1}, action){//initial value of state is assigne
     return state;
 }
 //console.log(store.getState());//action is not a part of redux , it is a convention that you have to send such type of objects{type:'increment'}
-const history = []
+// const history = []
 //store.subscribe(()=>{
 // history.push(store.getState());
  // console.log("History",history)
